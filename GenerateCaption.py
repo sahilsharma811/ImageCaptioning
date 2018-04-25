57a99b8ea563ec4a09a3cfee03c89acf98830721
sahilsharma811 <sahil8sharma8@gmail.com>
2018-04-25 18:20:08 +0530

    Filters Content - New Content

--- GenerateCaption.py
+++ GenerateCaption.py
@@ -1,29 +1,38 @@
-import cloudsight as CV
+import cloudsight as CS
 import GenerateVoice as GV
 import time
 def Caption(image_name, passs):
-	auth=CV.SimpleAuth(passs)
-	conn = CV.API(auth)	
+	auth = CS.SimpleAuth(passs)
+	conn = CS.API(auth)	
 	Filter_chocolate = ['chocolate', 'pizza']
 	filter_phone=['smartphone', 'phone', 'mobile', 'Smartphone', 'Phone' ,'Mobile']
+	filter_cigarette=['cigarette', 'cigarettes', 'smoking', 'smoke']
 	with open(image_name, 'rb') as f:
 		response = conn.image_request(f, image_name, {
 			'image_request[locale]': 'en-US',
 		})
 	status = conn.wait(response['token'], timeout=30)
 	status = conn.image_response(response['token'])
-	if status['status'] != cloudsight.STATUS_NOT_COMPLETED:
+	if status['status'] != CS.STATUS_NOT_COMPLETED:
 		try:
 			caption = status['name']
 			for content in Filter_chocolate:
 				if content in caption:
-					print("Warning: Eating %s is injurious to Health.." %content) 
-					GV.speak('Warning: Eating Cholocate is injurious to Health.... Image Content')
+					message = "Warning: Eating %s is injurious to Health...Image Content" %content 
+					GV.speak('Warning: Eating Cholocate is injurious to Health.... Image Content\n')
+					message = message+caption
+					caption=message
 					break
+			for content in filter_cigarette:
+				if content in caption:
+					message = "Warning: Smoking %s is injurious to Health...Image Content. " %content 
+					GV.speak('Warning: Smoking %s is injurious to Health.... Image Content. \n' %content)
+					message = message+caption
+					caption=message
+					break		
 			for content in filter_phone:
 				if content in caption:
 					caption = "Alert: %s Detected.. Filtered Content.." %content
-					
 			GV.speak(caption)
 			return(caption)
 		except:
@@ -32,4 +41,4 @@ def Caption(image_name, passs):
 		print('Cannot Process Image')
 
 if __name__ == '__main__':
-Caption('try.jpg','')
+	Caption('try.jpg', '')
